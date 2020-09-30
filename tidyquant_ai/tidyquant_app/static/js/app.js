b50f7ca11fd6c4293cecd65a5ea9e02a24d7b661
Anshoo <anshum45@gmail.com>
2020-09-30 10:22:59 +0530

    new commit

--- tidyquant_ai/tidyquant_app/static/js/app.js
+++ tidyquant_ai/tidyquant_app/static/js/app.js
@@ -6,3 +6,26 @@ ScrollReveal().reveal('.headline',{
     distance: '10px',
     origin :'left',
 });
+
+let list= ["text-mining","Natural Language Processing","Computer Vision"]
+let element = document.querySelector('.pop-up');
+let name = document.querySelector('.timeandcontent-pop-up');
+
+
+var myVar = setInterval(myTimer, 1000);
+var newvar = setInterval(print, 3000);
+
+function myTimer() {
+  var d = new Date();
+  var t = d.toLocaleTimeString();
+  element.innerHTML = t;
+
+}
+
+function print(){
+    list.forEach(item=>{
+        setTimeout(function(){name.innerHTML = item;  }, 1000);
+
+    })
+    }
+
